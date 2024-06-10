e96aa334d9342afa8117e88f570665be8b01860c
Yax Prajapati <92200237+YaxPrajapti@users.noreply.github.com>
2024-06-10 22:02:35 +0530

    deployment ready

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
-import express from "express"; 
-import dotenv from 'dotenv' 
+import path from 'path';
+import express from "express";
+import dotenv from 'dotenv'
 import morgan from "morgan";
-import cors from "cors"; 
+import cors from "cors";
 import cookieParser from 'cookie-parser'
-import {app, io, server} from './Socket/socket.js'; 
-
-dotenv.config({path: "../.env"}); 
+import { app, io, server } from './Socket/socket.js';
+import helmet from 'helmet'; 
+import compression from 'compression';
+dotenv.config({ path: "../.env" });
 
 //import local modules;
 import authRoutes from "./routes/auth.routes.js"
@@ -14,21 +16,30 @@ import userRoutes from './routes/user.routes.js'
 
 import connect from './databse/connectToDB.js'
 
+const __dirname = path.resolve();
 
 //env vars; 
-const PORT = process.env.PORT||5000; 
+const PORT = process.env.PORT || 5000;
 
-app.use(morgan('dev')); 
+app.use(morgan('dev'));
 app.use(cors());
-app.use(express.json()); 
-app.use(cookieParser()); 
+app.use(express.json());
+app.use(cookieParser());
+app.use(helmet()); 
+app.use(compression()); 
 
 //middlewares: 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/user', userRoutes);
 
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+})
+
 server.listen(PORT, () => {
-    connect(); 
+    connect();
     console.log(`Server started on ${PORT}`);
 })
